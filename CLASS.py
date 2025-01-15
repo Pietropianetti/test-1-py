e66aeab7eabe09461a5098f9b321f32044626b77
Pietropianetti <77731316+Pietropianetti@users.noreply.github.com>
2025-01-14 23:31:54 -0800

    Update CLASS.py
    dado

--- CLASS.py
+++ CLASS.py
@@ -1,9 +1,18 @@
+import json 
 import sys
-import xml.etree.ElementTree as ET
 
 
-class add():
-    print("add")
+class dado:
+	def  __init__ (self, Lista):
+		self.Lista = Lista
+	
+	def add(self, name):	
+	
+		lista = self.Lista
+		
+		with open(lista, "w") as file:
+			json.dump(name , file)
+			
 
 class remove():
     print("remove")
