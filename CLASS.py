c0d328d877a5522bea49d920cdb0ba888673441d
Pietropianetti <77731316+Pietropianetti@users.noreply.github.com>
2025-01-15 22:05:25 -0800

    Update CLASS.py

--- CLASS.py
+++ CLASS.py
@@ -3,16 +3,32 @@ import sys
 
 
 class dado:
-	def  __init__ (self, Lista):
-		self.Lista = Lista
+	def  __init__ (self, Lista , Name):
+		self.Lista = f"{Lista}.json"
+		self.Name = Name
 	
-	def add(self, name):	
-	
-		lista = self.Lista
-		
-		with open(lista, "w") as file:
-			json.dump(name , file)
-			
+	def add(self):
 
-class remove():
-    print("remove")
+		try:
+			with open(self.Lista, "r") as file:
+				
+				info = json.load(file)
+				print(infoq{)
+			
+		except FileNotFoundError: 
+				info = [ ]
+				
+		print(info)	
+			
+		info.append(self.Name ) 
+		
+		print(info)
+		
+		with open(self.Lista, "w") as file:
+			json.dump(info , file)
+	
+		print(self.Name)
+		print(self.Lista)
+		
+		
+dado("oi","d").add()
