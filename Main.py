a179182a3b4a5ae22e9b655bba00407f7470f6a2
Pietropianetti <77731316+Pietropianetti@users.noreply.github.com>
2025-01-18 10:56:03 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,26 @@
 import sys
-import CLASS
+import Oi
+
+class main:
+		def __init__(self):
+		  
+		  	acao = input("\n You would like to add or remove").upper()
+		  	
+		  	if acao == "ADD" :
+		  
+		  		list_Modify =  input("\n" "Which list do you want to modify/add : ").upper()
+		
+		  		name_add = input("what do you want to add(don't foget the coma : ").upper()
+          
+print("Welcome" "\n" 
+          "Let's star" "\n" 
+          "Do you want to add/remove something on the liste")
+    
+user = input("S for Yes and N to Nope : ").upper()
+
+if user == "S":
+    	
+    main()
+ 
 
 
-class main():
-
-    print("Welcome" "\n" "Let's star" "\n" "Do you want to add something on the liste")
-    inicio = input("S for Yes and N to Nope : ").upper()
-
-    while True:
-
-        if inicio == "S" :
-          list_Modify =  input("\n" "Wichi list do you want to modify/add : ").upper()
-        break
-
-main() 
-CLASS.add()
